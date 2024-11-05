33b312efe8ae76936ea992316fe0d8ecd15688d5
spoorthi967 <gopuspoorthi18@gmail.com>
2024-11-05 18:12:02 -0500

    add WidgetView.js

---
+++ src/pages/dashboard/WidgetView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WidgetView() {
+  return (
+    <div className='text-white flex justify-center items-center w-full h-screen'>
+        <h1 className='text-4xl'> WidgetView page</h1>
+    </div>
+  )
+}
+
+export default WidgetView
